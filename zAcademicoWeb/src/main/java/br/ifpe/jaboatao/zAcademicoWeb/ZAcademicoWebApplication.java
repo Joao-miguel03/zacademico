f7f6ce2c850273e49bf9ebd3f0030111bd880126
Joao-miguel03 <ser.joaomiguel@gmail.com>
2025-01-27 09:58:47 -0300

    Add files via upload

--- zAcademicoWeb/src/main/java/br/ifpe/jaboatao/zAcademicoWeb/ZAcademicoWebApplication.java
+++ zAcademicoWeb/src/main/java/br/ifpe/jaboatao/zAcademicoWeb/ZAcademicoWebApplication.java
@@ -2,8 +2,10 @@ package br.ifpe.jaboatao.zAcademicoWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.ifpe.jaboatao.zAcademicoWeb.spring_boot","br.ifpe.jaboatao.zAcademicoWeb.model.dao"})
 public class ZAcademicoWebApplication {
 
 	public static void main(String[] args) {
--- zAcademicoWeb/src/main/java/br/ifpe/jaboatao/zAcademicoWeb/spring_boot/AlunoController.java
+++ zAcademicoWeb/src/main/java/br/ifpe/jaboatao/zAcademicoWeb/spring_boot/AlunoController.java
@@ -2,15 +2,12 @@ package br.ifpe.jaboatao.zAcademicoWeb.spring_boot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.ifpe.jaboatao.zAcademicoWeb.model.Aluno;
 import br.ifpe.jaboatao.zAcademicoWeb.model.dao.AlunoDAO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -21,15 +18,15 @@ public class AlunoController {
         this.alunoDAO = alunoDAO;
     }
 
-    @GetMapping("aluno/inserir")
+    @GetMapping("/aluno/inserir")
     public String abreForm(Model model) {
         model.addAttribute("aluno", new Aluno(null, null, 0, null, null, null, null));
-        return "aluno/inserir";
+        return "cadAluno";
     }
     
 
     //@RequestMapping(value="/aluno/inserir", method = RequestMethod.POST)
-    @PostMapping("aluno/inserir")
+    @PostMapping("/aluno/inserir")
     public String insereAluno(@ModelAttribute Aluno aluno, Model model){
         alunoDAO.create(aluno);
 
